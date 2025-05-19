fdc5461db3da0349413a8d061e5c3e9a072f909f
EATLEO <leo.rc3403@gmail.com>
2025-05-19 22:47:05 +0900

    Fix postcss nesting config

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'tailwindcss/nesting': {}, // nesting 먼저
+    'postcss-nesting': {},
     tailwindcss: {},
     autoprefixer: {},
   },
-};
+}
