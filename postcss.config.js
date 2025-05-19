0221b0544b4c0579d888707a70cc379766456b5d
EATLEO <leo.rc3403@gmail.com>
2025-05-19 22:43:53 +0900

    Fix postcss config for tailwindcss on Vercel

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
-// postcss.config.js
 module.exports = {
   plugins: {
-    'tailwindcss': {},
-    'autoprefixer': {},
+    'postcss-import': {},
+    'tailwindcss/nesting': {}, // nesting 먼저
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
